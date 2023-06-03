7906d62dfffe165ea23830a39ddb7bf9f6415514
MFAIZAN20 <112976979+MFAIZAN20@users.noreply.github.com>
2023-06-03 16:53:35 +0500

    Update Lab_Task_1_Question_2.cpp

--- Lab_Task_1_Question_2.cpp
+++ Lab_Task_1_Question_2.cpp
@@ -1,2 +1,19 @@
-This File Contains All My Lab Task That I Have Done 
-/* Object Oriented LAb */
+#include <iostream>
+using namespace std;
+
+int main(){
+float weight,height,x;
+float BMI;
+    cout<<"Please Enter Your Weight:"<<endl;
+    cin>>weight;
+
+    cout<<"Please Enter Your Height:"<<endl;
+    cin>>height;
+
+    x=height*height;
+
+    BMI=weight/x;
+    
+    cout<<"Your BMI is:"<<(float)BMI<<endl;
+return 0;
+}
