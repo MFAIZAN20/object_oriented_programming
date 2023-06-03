e1a9bc8664eacd79d1714f34c2855ab05046dfce
MFAIZAN20 <112976979+MFAIZAN20@users.noreply.github.com>
2023-06-03 17:13:05 +0500

    Add files via upload

---
+++ lab_task_1/Muhammad_Faizan_p229138.task_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+float weight,height,x;
+float BMI;
+    cout<<"Please Enter Your Weight:"<<endl;
+    cin>>weight;
+
+    cout<<"Please Enter Your Height:"<<endl;
+    cin>>height;
+
+    x=height*height;
+
+    BMI=weight/x;
+    
+    cout<<"Your BMI is:"<<(float)BMI<<endl;
+}
